c5dc9d8a62920f1c341c14dde6a87d9983ae0faf
Maarten Menten <mentenmaarten@gmail.com>
2018-09-04 12:39:23 +0200

    changed if statement structure

--- single.php
+++ single.php
@@ -19,9 +19,10 @@ get_header();
 				get_template_part( 'template-parts/content', get_post_type() );
 
 				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
+				if ( comments_open() || get_comments_number() )
+				{
 					comments_template();
-				endif;
+				}
 
 				the_post_navigation( array
 				(
