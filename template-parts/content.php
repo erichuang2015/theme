503df77b0f9e74a97e37ba7346224324fd36c73d
Maarten Menten <mentenmaarten@gmail.com>
2018-09-11 18:18:46 +0200

    - added spaces

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,20 +9,31 @@
 	
 	<header class="entry-header">
 		<?php
-		if ( 'post' === get_post_type() ) {
+
+		if ( 'post' === get_post_type() ) 
+		{
 			echo '<div class="entry-meta">';
+
 			if ( is_single() ) 
 			{
 				theme_posted_on();
 			};
+
 			echo '</div><!-- .entry-meta -->';
 		};
 
-		if ( is_single() ) {
+		if ( is_single() ) 
+		{
 			the_title( '<h1 class="entry-title">', '</h1>' );
-		} elseif ( is_front_page() && is_home() ) {
+		} 
+
+		elseif ( is_front_page() && is_home() ) 
+		{
 			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-		} else {
+		} 
+
+		else 
+		{
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		}
 
